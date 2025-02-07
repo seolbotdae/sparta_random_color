8dbf2da9d4bb942452245885b0eefcbf73893616
seolbotdae <seolbotdae@gmail.com>
2025-02-07 15:11:26 +0900

    Initial Commit

---
+++ RandomColor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RandomColor
+//
+//  Created by Seol WooHyeok on 2/7/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
